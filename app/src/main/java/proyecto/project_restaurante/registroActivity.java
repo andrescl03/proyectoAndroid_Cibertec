32874bcf1229aead8c5af7885508f432da9a50f9
andrescl03 <andrescarrion199603@gmail.com>
2018-11-04 16:45:29 -0500

    Diseño Basico de la interfaz principal

---
+++ app/src/main/java/proyecto/project_restaurante/registroActivity.java
@@ -0,0 +1,13 @@
+package proyecto.project_restaurante;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
