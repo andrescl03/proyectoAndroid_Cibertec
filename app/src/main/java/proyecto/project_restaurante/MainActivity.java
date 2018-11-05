8f946f87de1150e1f27d27f575a2dd06db0b8d1d
andrescl03 <andrescarrion199603@gmail.com>
2018-11-05 02:15:52 -0500

    Se aplico el sharedPreferences para el CheckBox "Recordar Sesión".

--- app/src/main/java/proyecto/project_restaurante/MainActivity.java
+++ app/src/main/java/proyecto/project_restaurante/MainActivity.java
@@ -1,13 +1,17 @@
 package proyecto.project_restaurante;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.prefs.Preferences;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     Button btnIniciarSesion;
     EditText txtCorreo;
     EditText txtClave;
+    CheckBox chkGuardarSesion;
 
     Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 
@@ -29,10 +34,31 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
         btnRegistrarse.setOnClickListener(this);
         btnIniciarSesion.setOnClickListener(this);
-
-
     }
 
+    protected void onResume(){
+        super.onResume();
+        String datoTemporalCorreo;
+        String datoTemporalClave;
+        boolean datoTemporalCheckBox;
+        SharedPreferences datos = PreferenceManager.getDefaultSharedPreferences(this);
+
+        datoTemporalCorreo = datos.getString("usuario","");
+        datoTemporalClave = datos.getString("clave","");
+        datoTemporalCheckBox = datos.getBoolean("checkBox",false);
+        txtCorreo.setText(datoTemporalCorreo);
+        txtClave.setText(datoTemporalClave);
+        chkGuardarSesion.setChecked(datoTemporalCheckBox);
+
+    }
+    public void llamadaComponentes(){
+
+        txtCorreo = findViewById(R.id.txtCorreo);
+        txtClave = findViewById(R.id.txtClave);
+        btnRegistrarse = findViewById(R.id.btnRegistrarse);
+        btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+        chkGuardarSesion = findViewById(R.id.chkGuardarSesion);
+    }
 
     @Override
     public void onClick(View v) {
@@ -45,16 +71,31 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
                     break;
             case R.id.btnIniciarSesion:
                     if(txtCorreo.getText().toString().trim().isEmpty() || txtClave.getText().toString().trim().isEmpty()){
-                         Toast.makeText(this,"Campo Vacio",Toast.LENGTH_SHORT).show();
+                         Toast.makeText(this,"Campos Vacios",Toast.LENGTH_SHORT).show();
                 }
                     else{
-
                         if(txtCorreo.getText().toString().matches(pattern.toString())) {
-                            Toast.makeText(this, "Registro Exitoso", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(this, "Login Exitoso", Toast.LENGTH_SHORT).show();
+                            SharedPreferences datos = PreferenceManager.getDefaultSharedPreferences(this);
+                            SharedPreferences.Editor editor = datos.edit();
+                                    //AQUI LLAMAR AL SIGUIENTE ACITIVITY
+                            
+                                if(chkGuardarSesion.isChecked()){
+                                    editor.putString("usuario", txtCorreo.getText().toString());
+                                    editor.putString("clave", txtClave.getText().toString());
+                                    editor.putBoolean("checkBox", chkGuardarSesion.isChecked());
+                                    editor.apply();
+
+                                }
+                                else{
+                                    datos.edit().remove("usuario").commit();
+                                    datos.edit().remove("clave").commit();
+                                    datos.edit().remove("checkBox").commit();
+                                }
                         }
                         else{
                             Toast.makeText(this,"Coloque un correo valido",Toast.LENGTH_SHORT).show();
-                        }
+                            }
                         }
                     break;
 
@@ -64,13 +105,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
 
 
-    public void llamadaComponentes(){
 
-        txtCorreo = findViewById(R.id.txtCorreo);
-        txtClave = findViewById(R.id.txtClave);
-        btnRegistrarse = findViewById(R.id.btnRegistrarse);
-        btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
-    }
 
 
 
