364170e34ed5dcf79ab1376744226899676cb829
andrescl03 <andrescarrion199603@gmail.com>
2018-11-05 23:39:59 -0500

    Agregando Iconos y modificando Layouts.

---
+++ app/src/main/java/proyecto/project_restaurante/entidades/Usuario.java
@@ -0,0 +1,88 @@
+package proyecto.project_restaurante.entidades;
+
+public class Usuario {
+
+        private Integer idUsuario;
+        private String nombres;
+        private String apellidos;
+        private String correo;
+        private String clave;
+        private String edad;
+        private boolean sexo;
+        private String tokenUsuario;
+
+    public Usuario(Integer idUsuario, String nombres, String apellidos, String correo, String clave, String edad, boolean sexo, String tokenUsuario) {
+        this.idUsuario = idUsuario;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.clave = clave;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.tokenUsuario = tokenUsuario;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public boolean isSexo() {
+        return sexo;
+    }
+
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTokenUsuario() {
+        return tokenUsuario;
+    }
+
+    public void setTokenUsuario(String tokenUsuario) {
+        this.tokenUsuario = tokenUsuario;
+    }
+}
--- app/src/main/java/proyecto/project_restaurante/registroActivity.java
+++ app/src/main/java/proyecto/project_restaurante/registroActivity.java
@@ -2,12 +2,26 @@ package proyecto.project_restaurante;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class registroActivity extends AppCompatActivity {
 
+        Spinner spnSexo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        spnSexo = findViewById(R.id.spnSexo);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.combo_sexo,android.R.layout.simple_spinner_item);
+        spnSexo.setAdapter(adapter);
+
+
     }
+
+
+
+
 }
